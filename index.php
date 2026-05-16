232b18a45e7ccb433e77abf07ee1145d0bd22b64
sahil <sahil.dhiman@theknowledgeacademy.com>
2026-05-16 14:02:16 +0530

    done

--- index.php
+++ index.php
@@ -1,15 +1,15 @@
 <?php
 
-// $host = "localhost";
-// $user = "root";
-// $password = "";
-// $database = "MVS";
+$host = "localhost";
+$user = "root";
+$password = "";
+$database = "MVS";
 
-// $conn = mysqli_connect($host, $user, $password, $database);
+$conn = mysqli_connect($host, $user, $password, $database);
 
-// if (!$conn) {
-//     die("Connection failed: " . mysqli_connect_error());
-// }
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
 
 echo "Database connected successfully!";
 
